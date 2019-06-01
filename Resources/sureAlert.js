98281bba7250d881a02330b2c2ce0cdc98499bed
giovani <18gio12>
2019-06-01 18:23:20 -0300

    páginas da conta criadas ou alteradas

---
+++ Resources/sureAlert.js
@@ -0,0 +1,13 @@
+function sureAlert() {
+    var txt;
+    if (confirm("Tem certeza disso?")) {
+        alert("Inscrição enviada com sucesso!");
+    }
+}
+
+function sureAlertCancelamento() {
+    var txt;
+    if (confirm("Tem certeza disso?")) {
+        alert("Matrícula cancelada com sucesso!");
+    }
+}
